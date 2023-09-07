62a8c21b8faa7057cd81f3f24bbf5df6e7d0e651
Md:Kawsar Ahmed <knownaskawsar@gmail.com>
2023-09-07 21:39:25 +0600

    fileUp

---
+++ script.js
@@ -0,0 +1,212 @@
+$('.alert-personalweb').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">🦸‍♂️ Kawsar\'s Personal Website</h1>',
+    imageUrl: 'assets/logo-z-personalweb.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://curl-kawsar.github.io/personal">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-linkedin').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">👨‍🎓 LinkedIn :\n Curriculum Vitae</h1>',
+    imageUrl: '@original/assets/logo-linkedin.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://www.linkedin.com/in/curl-kawsar">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-kaggle').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">🙋‍♂️ :\n Data Science and Machine Projects</h1>',
+    imageUrl: '@original/assets/logo-kaggle.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://www.kaggle.com/curl-kawsar">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-github').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">👨‍💻 GitHub :\n Web-Based Projects</h1>',
+    imageUrl: '@original/assets/logo-github.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://github.com/curl-kawsar">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-codepen').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">👨‍💻 Codepen :\n Frontend Projects</h1>',
+    imageUrl: '@original/assets/logo-codepen.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://codepen.io/curl-kawsar">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-behance').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">👨‍🎨 Behance :\n Design & Illustration</h1>',
+    imageUrl: '@original/assets/logo-behance.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://www.behance.net/curl-kawsar">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-dribbble').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">👨‍🎨 Dribbble :\n Design & Illustration</h1>',
+    imageUrl: '@original/assets/logo-dribbble.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://dribbble.com/curl-kawsar">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+// =============================================================================================== //
+// =============================================================================================== //
+
+$('.alert-medium').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">✍️ Medium :\n Insight Stories</h1>',
+    imageUrl: '@original/assets/logo-medium.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://medium.com/@knownaskawsar/">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-spotify').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">🎙️ Spotify :\n Podcast Everyday</h1>',
+    imageUrl: '@original/assets/logo-spotify.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://medium.com/@knownaskawsar">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-youtube').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">🎬 Youtube :\n Subscribe Now</h1>',
+    imageUrl: '@original/assets/logo-youtube.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://www.youtube.com/@FoldCurl-hh4dd">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-discord').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">💬 Discord : \n Join Now</h1>',
+    imageUrl: '@original/assets/logo-discord.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://discord.gg/RXFCb5F5gm">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-instagram').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">🎯 Instagram :\n Microblog Plus</h1>',
+    imageUrl: '@original/assets/logo-instagram.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="http://instagram.com/justin_kawchar">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-twitter').click(function () {
+  Swal.fire({
+    title: '<h1 style="font-size: 85%; text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2)">🎭 Twitter</h1>',
+    imageUrl: '@original/assets/logo-twitter.svg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://twitter.com/curl-kawsar">Visit</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+
